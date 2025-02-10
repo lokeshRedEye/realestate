3dcb612d82f4f95a163b8bc59ab0edbc102c6b36
lokeshRedEye <lokeshcoder123@gmail.com>
2025-02-10 09:57:52 +0530

    navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import "./navbar.scss";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import { useNotificationStore } from "../../lib/notificationStore";
+
 function Navbar() {
   const [open, setOpen] = useState(false);
 
@@ -28,20 +29,17 @@ function Navbar() {
       <div className="right">
         {currentUser ? (
           <div className="user">
-            <img
-              src = {currentUser.avatar  || '/noavatar.jpg'}
-              alt=""
-            />
+            <img src={currentUser.avatar || "/noavatar.jpg"} alt="" />
             <span>{currentUser.username}</span>
             <Link to="/profile" className="profile">
-            {number > 0 && <div className="notification">{number}</div>}
+              {number > 0 && <div className="notification">{number}</div>}
               <span>Profile</span>
             </Link>
           </div>
         ) : (
           <>
             <a href="/login">Sign in</a>
-            <a href="/register" className="profile-register" >
+            <a href="/register" className="register">
               Sign up
             </a>
           </>
